6ef0c0d889f8773ee274918de3424d004ed5b8d7
Emeraldnickel <Emeraldnickel@gmail.com>
2021-04-29 09:13:31 +1000

    Regenerated angular workspace, beginning reactive form

--- SEEKsurvey/src/app/app.module.ts
+++ SEEKsurvey/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, NameEditorComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+  }
+}
